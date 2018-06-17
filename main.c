e596ae84e8c4dc0ff29309411aa64356efa5c278
metalm <felipetiagodecarli@hotmail.com>
2018-06-17 16:31:51 -0300

    add constants

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+
+
+#define NUM_PIECES 106
+#define NUM_COLOR 4
+#define NUM_COLOR_PIECES 26
+#define NUM_JOKER 2
+
+
+const char *blue_piece[] = {"1!", "2!", "3!", "4!",
+				 "5!", "6!", "7!",
+				"8!", "9!", "A!",
+				"B!", "C!", "D!"};
+
+const char *yellow_piece[] = {"1@", "2@", "3@", "4@",
+				"5@", "6@", "7@",
+				"8@", "9@", "A@",
+				"B@", "C@", "D@"};
+
+const char *black_piece[] = {"1#", "2#", "3#", "4#",
+				"5#", "6#", "7#",
+				"8#", "9#", "A#",
+				"B#", "C#", "D#"};
+
+const char *red_piece[] = {"1$", "2$", "3$", "4$",
+				"5$", "6$", "7$",
+				"8$", "9$", "A$",
+				"B$", "C$", "D$"};
+
+const char *joker[] = {"**", "**"}
+
+
+int main (int argc, char *argv[]) {
+	return 0;
+}
