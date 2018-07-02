aa649b74ccad3cba9c797f1badd496c17dff4210
metalm <felipetiagodecarli@hotmail.com>
2018-07-02 18:14:15 -0300

    arruma insert set

--- tests/set_tests.c
+++ tests/set_tests.c
@@ -12,18 +12,19 @@ int main (void) {
     Set *test_set2 = NULL;
     Set *test_set3 = NULL;
     Set *test_set4 = NULL;
-    Set *test_set5 = NULL;
+    /* Set *test_set5 = NULL; */
     char *test_pieces[] = {"1!", "1@", "1#", "**"};
     char *test_pieces_2[] = {"4$", "4#", "4@", "4!"};
     char *test_pieces_3[] = {"3$", "3!", "**", "**"};
     char *test_pieces_4[] = {"9!", "A!", "B!", "C!"};
-    char *test_pieces_5[] = {"**", "**", "C!",  "D!"};
+    /* char *test_pieces_5[] = {"**", "**", "C!",  "D!"}; */
+    char *test_pieces_6[] = {"1$"};
     test_set1 = new_set (test_set1, false, test_pieces, 4);
-    test_set2 = new_set (test_set2, false, test_pieces_2, 4);
-    test_set3 = new_set (test_set3, false, test_pieces_3, 4);
-    test_set4 = new_set (test_set4, true, test_pieces_4, 4);
-    test_set5 = new_set (test_set5, true, test_pieces_5, 4);
-    test_set1 = new_set (test_set1, true, test_pieces_5, 4);
+    /* test_set2 = new_set (test_set2, false, test_pieces_2, 4); */
+    /* test_set3 = new_set (test_set3, false, test_pieces_3, 4); */
+    /* test_set4 = new_set (test_set4, true, test_pieces_4, 4); */
+    insert_in_set (test_set1, test_set1->run, test_pieces_6, 1); 
+    /* test_set1 = new_set (test_set1, true, test_pieces_5, 4); */
     if (test_set1 == NULL) {
         printf ("Conjunto 1 nao passou no teste\n");
     }
